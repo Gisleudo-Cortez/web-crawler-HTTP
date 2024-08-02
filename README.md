a195b23436163d8b3476438723344759045d4167
Gisleudo <gisleudo.cmf@gmail.com>
2024-08-02 14:39:20 -0300

    Implement and test getURLsFromHTML, fix README.md format

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 Build a Web Crawler in JS
 
-    Use JS to build a web crawler that any SEO expert would be happy to have. You'll make HTTP requests and parse HTML to generate reports that can easily marshaled to standard output or a file. If you're interested in getting a job using Node.js, this project will teach you how Node can be used to build a command-line application. 
+Use JS to build a web crawler that any SEO expert would be happy to have. You'll make HTTP requests and parse HTML to generate reports that can easily marshaled to standard output or a file. If you're interested in getting a job using Node.js, this project will teach you how Node can be used to build a command-line application. 
--- crawl.test.js
+++ crawl.test.js
@@ -1,6 +1,8 @@
-import { normalizeURL } from './crawl.js';
+import { normalizeURL, getURLsFromHTML } from './crawl.js';
 import { test, expect } from '@jest/globals';
 
+
+// normalizeURL tests
 
 test('normalizeURL strip protocol', () => {
     const input = 'https://blog.boot.dev/path'
@@ -27,5 +29,60 @@ test('normalizeURL strip http', () => {
     const input = 'http://blog.boot.dev/path/'
     const actual = normalizeURL(input)
     const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+// getURLsFromHTML tests
+
+test('getURLsFromHTML absolute', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="https://blog.boot.dev/path/"><span>Go to Boot.dev</span></a>
+    </body>
+</html>`
+    const inputBaseURL = 'https://blog.boot.dev/path/'
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ['https://blog.boot.dev/path/']
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML relative', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="/path/"><span>Go to Boot.dev</span></a>
+    </body>
+</html>`
+    const inputBaseURL = 'https://blog.boot.dev'
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ['https://blog.boot.dev/path/']
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML both', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="https://blog.boot.dev/path1/"><span>Go to Boot.dev path one</span></a>
+        <a href="/path2/"><span>Go to Boot.dev path two</span></a>
+    </body>
+</html>`
+    const inputBaseURL = 'https://blog.boot.dev'
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ['https://blog.boot.dev/path1/', 'https://blog.boot.dev/path2/']
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML Invalid URL', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="invalid"><span>Invalid URL</span></a>
+    </body>
+</html>`
+    const inputBaseURL = 'https://blog.boot.dev'
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = []
     expect(actual).toEqual(expected)
 })
