6f51ebb33a48ddee97c030613a78eebb0a01d2e5
Gisleudo <gisleudo.cmf@gmail.com>
2024-08-05 10:42:33 -0300

    Implement crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,29 @@
 import { JSDOM } from 'jsdom';
 
+
+async function  crawlPage(currentURL){
+    console.log(`actively crawling ${currentURL}`)
+
+    try {
+        const resp = await fetch(currentURL)
+        
+        if (resp.status > 399){
+            console.log(`error in fetch with status code ${resp.status} on page ${currentURL}`)
+            return
+        }
+
+        const contentType = resp.headers.get('content-type')
+        if (!contentType.includes('text/html')){
+            console.log(`non html response, content type ${contentType} on page ${currentURL}`)
+            return
+        }
+        
+        console.log(await resp.text())
+        } catch(err){
+        console.log(`error in fetch ${err.message}, on page ${currentURL}`)
+    }
+}
+
 function normalizeURL(urlString){
     const urlObj = new URL(urlString);
     const hostpath = `${urlObj.hostname}${urlObj.pathname}`
@@ -38,5 +62,6 @@ function getURLsFromHTML(htmlBody, baseURL){
 
 export {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 };
